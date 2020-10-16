2c3ca9c00c188643c33ac0bf9c80ab026d1480f7
MohamedAmineDerouiche <mohderouiche250881@gmail.com>
2020-10-16 13:14:24 +0100

    get and post req ok from mongodb/

--- react-client/src/components/AddProduct.jsx
+++ react-client/src/components/AddProduct.jsx
@@ -1,17 +1,35 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 export default class AddProduct extends Component {
     constructor(props){
-        super(props)
-        
+        super(props);
+        this.state={
+            name: '',
+            size: '',
+            price: 0
+        };
+        this.saveProduct = this.saveProduct.bind(this);
+        this.sendNewProduct = this.sendNewProduct.bind(this)  
+    }
+    saveProduct(e){
+        this.setState({
+            [e.target.name] : e.target.value
+        })
+        console.log(this.state)
+    }
+    sendNewProduct(){
+        const {name,size,price} = this.state
+        axios.post('http://localhost:5000/newproducts',{name,size,price})
     }
     render() {
+        const {name,size,price} = this.state
         return (
             <div>
-                <input type="text"/>
-                <input type="text"/>
-                <input type="text"/>
-                 <button  >add new product</button>
+                <input type="text" placeholder="name of outfit" name='name' onChange={this.saveProduct}/>
+                <input type="text" placeholder="size" name="size" onChange={this.saveProduct}/>
+                <input type="text" placeholder="suggested price" name="price" onChange={this.saveProduct}/>
+                 <button onClick={this.sendNewProduct} >add new product</button>
                 
             </div>
         )
